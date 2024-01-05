8df0b45830331075720cc86fdbb3a5bb3b6b16fb
Ludmi <ludmit@outlook.com>
2024-01-05 14:15:22 -0300

    feat: :sparkles: get book titles

---
+++ src/guzzle_requests.php
@@ -0,0 +1,18 @@
+<?php
+# Url to scrape: https://books.toscrape.com/
+require 'vendor/autoload.php';
+$httpClient = new \GuzzleHttp\Client();
+$response = $httpClient -> get('https://books.toscrape.com/');
+$htmlString = (string) $response -> getBody();
+// Supress any warnings
+libxml_use_internal_errors(true);
+$doc = new DOMDocument();
+$doc->loadHTML($htmlString);
+$xpath = new DOMXPath($doc);
+
+$titles = $xpath->evaluate('//ol[@class="row"]//li//article//h3//a');
+$extractedTitles = [];
+foreach ($titles as $title) {
+    $extractedTitles[] = $title -> textContent.PHP_EOL;
+    echo $title -> textContent.PHP_EOL;
+}
