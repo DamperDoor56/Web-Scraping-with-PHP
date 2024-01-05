62dee169ff8a5491b95325507638688967ca60b4
Ludmi <ludmit@outlook.com>
2024-01-05 15:27:53 -0300

    feat: :sparkles: update css request, add description

--- src/goutte_css_requests.php
+++ src/goutte_css_requests.php
@@ -5,12 +5,20 @@ $httpClient = new \Goutte\Client();
 $response = $httpClient->request('GET', 'https://books.toscrape.com/');
 // get prices into an array
 $prices = [];
-$response->filter('.row li article div.product_price p.price_color')->each(function ($node) use (&$prices) {
+$response->filter('.row li article div.product_price p.price_color')
+->each(function ($node) use (&$prices) {
     $prices[] = $node->text();
 });
-// echo titles and prices
+// echo titles and prices, and description
 $priceIndex = 0;
-$response->filter('.row li article h3 a')->each(function ($node) use (&$prices, &$priceIndex) {
-    echo $node->text() . ' @ ' . $prices[$priceIndex] .PHP_EOL;
+$response->filter('.row li article h3 a')
+->each(function ($node) use (&$prices, &$priceIndex, $httpClient) {
+    $title = $node->text();
+    $price = $prices[$priceIndex];
+    // getting the decription
+    $description = $httpClient->click($node->link())
+    ->filter('.content #content_inner article p')->eq(3)->text();
+    // display the result
+    echo "{$title} @ {$price} : {$description}\n\n";
     $priceIndex++;
 });
